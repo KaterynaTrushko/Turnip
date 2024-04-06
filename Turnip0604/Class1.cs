7c2ccc3ca20cf9211dfd26bf6e9552c96a698d0b
Kateryna Trushko <trushkokateryna@gmail.com>
2024-04-06 15:13:11 +0100

    Add project files.

---
+++ Turnip0604/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Turnip0604
+    {
+    public interface ICharacter
+        {
+        public int Pawer { get; set; }
+        public string Name { get; set; }
+        public void CallForHelp(Character obj);
+        public void Pull();
+        
+        }
+
+    public class Character : ICharacter
+        {
+        public int Pawer { get; set; }
+        public string Name { get; set; }
+        public Character(string name, int pawer) 
+            {
+            this.Name = name;
+            this.Pawer = pawer;
+            }
+
+        public void Pull() 
+            {
+            Console.WriteLine("Pull!");
+            }
+        public void CallForHelp(Character obj) 
+            {
+            Console.WriteLine($"{obj} come here to help!");
+            }
+
+        public override string ToString() 
+            {
+            return Name;
+            }
+
+        }
+
+    public class Plant
+        {
+        public int Pawer { get; set; }
+        public string Name { get; set; }
+        public Plant(string name, int pawer)
+            {
+            this.Name = name;
+            this.Pawer = pawer;
+            }
+        }
+
+    public class Turnip : Plant
+        {
+        public Turnip(string name, int pawer) : base(name, pawer)
+            {
+            this.Name = name;
+            this.Pawer = pawer;
+            }
+        }
+    }
---
+++ Turnip0604/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Turnip0604
+    {
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            var characters = new List<Character>();
+            characters.Add(new Character("grandpa", 40));
+            characters.Add(new Character("grandma", 30));
+            characters.Add(new Character("granddaughter", 20));
+            characters.Add(new Character("dog", 10));
+
+            Turnip turnip = new("turnip", 110);
+
+            int accumulator = 0;
+            for(var i = 0; i < characters.Count; i++) 
+                {
+                accumulator += characters[i].Pawer;
+                if (accumulator < turnip.Pawer)
+                    {
+                    if((i + 1 ) < characters.Count) characters[i].CallForHelp(characters[i + 1]);
+                    }
+                else {
+                    Character newGiro = new("nextGiro", 20);
+                    }
+                }
+
+            Plant newPlan = new Plant ("tomato", 10);
+            }
+        }
+    }
